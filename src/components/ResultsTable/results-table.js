7eb0ccdbc22a71502012ca3e496f0b78bcbc9a6f
Trevor Howard <trevorhoward2@hotmail.com>
2021-11-02 18:52:14 -0600

    Quick styling for search and results components

--- src/components/ResultsTable/results-table.js
+++ src/components/ResultsTable/results-table.js
@@ -5,32 +5,32 @@ const ResultsTable = ({ data, hasSearched }) => {
     return (
         <>
             {hasSearched && data.length === 0 && (
-                <div>No results found, check your search and try again.</div>
+                <div className="no-results">No results found, check your search and try again.</div>
             )}
             {data.length > 1 && (
                 <section id="city-data" className="city-data" key={data}>
                     <div className='table'>
                         <h2>Results</h2>
-                        <table border={2} cellPadding={5}>
+                        <table border={2} cellPadding={10} cellSpacing={0}>
                             <thead>
                                 <tr>
                                     <td>ID</td>
                                     <td>Name</td>
                                     <td>State</td>
                                     <td>Country</td>
-                                    <td>Coordinates</td>
+                                    <td>Coordinates (lat, long)</td>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
                                     Object.keys(data).map(element => {
                                         return (
-                                            <tr key={element}>
+                                            <tr key={element} className="result-row">
                                                 <td>{data[element].id}</td>
                                                 <td>{data[element].name}</td>
                                                 <td>{data[element].state}</td>
                                                 <td>{data[element].country}</td>
-                                                <td>{data[element].coord.lon}, {data[element].coord.lat}</td>
+                                                <td>{data[element].coord.lat}, {data[element].coord.lon}</td>
                                             </tr>
                                         )
                                     })
